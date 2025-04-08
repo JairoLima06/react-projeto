b42f3f28c2b451175747e1c91df81f7f8158a3fc
JairoLima06 <francisco.jairodiasdelima@gmail.com>
2025-04-08 14:10:36 -0300

    Finalização do projeto

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import style from './Botao.module.scss';
 
+
+
 type Props = {
+  type?: "submit" | "reset" | "button";
+  onClick?: () => void;
   children: React.ReactNode;
 };
 
 class Botao extends React.Component<Props> {
   render() {
+    const { type = "button", onClick, children } = this.props;
     return (
-      <button className={style.botao}>
-        {this.props.children}
+      <button type={type} onClick={onClick} className={style.botao}>
+        {children}
       </button>
     );
   }
